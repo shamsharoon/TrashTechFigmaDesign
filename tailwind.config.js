a620e8ce49465b4883be10a98f066f22f941529b
shamsharoon <shamsuddin.haroon@ontariotechu.net>
2024-06-09 21:15:22 -0400

    added tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        // Adding a custom font family
+        sans: ['Outfit', 'sans-serif'], // You can replace 'Roboto' with your font name
+      },
+    },
+  },
+  plugins: [],
+}
+
